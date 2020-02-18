ed15b95fe203d7f176074ed24c558b35cf416200
besjb <jean-baptiste.bes@etu.umontpellier.fr>
2020-02-18 18:03:19 +0100

    ajout du premier test (compte 2 gagnant)

--- src/test/java/fr/umontpellier/iut/IBailleTest.java
+++ src/test/java/fr/umontpellier/iut/IBailleTest.java
@@ -1,17 +1,36 @@
 package fr.umontpellier.iut;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class IBailleTest {
+    private Compte c1,c2,c3;
+    private Produit p;
 
-    @Disabled
-    @Test
-    public void test_if_main_method_was_implemented() {
+    @BeforeEach
+    public void init(){
+        p = new Produit(1,"Produit",10);
 
-        assertDoesNotThrow(() -> IBaille.main(new String[1]));
+        c1 = new Compte("c1","emailC1","adreC1",0);
+        c2 = new Compte("c2","emailC2","adreC2",0);
+        c3 = new Compte("c3","emailC3","adreC3",0);
 
+        c1.crediter(1000);
+        c2.crediter(2500);
+        c3.crediter(1500);
+
+        p.demarrerEnchere(10);
+        p.setPasEnchere(1);
     }
-
+    //@Disabled
+    @Test
+    public void test_gagnant_c2() {
+        p.ajouterOffre(c1.creerOffre(p,12,100));
+        p.ajouterOffre(c2.creerOffre(p,15,200));
+        assertEquals(c2.getListeOffre(0),p.getGagnant());
+    }
 }
